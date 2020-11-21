15abdcc5d2fe146dc15aa4aac863008af709c1da
蘑菇QAQ <67809394+mogumc@users.noreply.github.com>
2020-11-21 15:43:23 +0800

    Add files via upload

--- ex.js
+++ ex.js
@@ -11,4 +11,5 @@
 // @updateURL https://ex.mo23.me/ex.js
 // @grant none
 // ==/UserScript==
+
 (function(){function panda_init(c){if(c>=3){return;};let n=['https://expanda.now.sh/','https://ghcdn.rawgit.org/noprogramming/expanda/master/','https://noprogramming.github.io/expanda/'];let t=setTimeout(function(){clearTimeout(t);panda_init(c+1);},3000);let s=document.createElement('script');s.src=(n[c]?n[c]:n[0])+'panda.js?'+parseInt(Date.parse(new Date())/600000)+c;s.onerror=function(){clearTimeout(t);panda_init(c+1);};s.onload=function(){clearTimeout(t);};s.setAttribute('exkey','c85b32b04583bdf636cf3b87c46f73be5543239x576a58691');document.body.appendChild(s);};panda_init(0);}());
